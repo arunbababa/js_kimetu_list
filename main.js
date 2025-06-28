00da31c42d2a79680ab80f213b28143f1a6bce4d
arunbababa <hatuki.1.gzs@icloud.com>
2025-06-28 16:58:58 +0900

    HTMLのイベント属性を受け取ることで関数の共通化

--- main.js
+++ main.js
@@ -1,38 +1,58 @@
-// 前データのクリアも各かんすうの前にセット
-
 // ローディング実装
 const handleLoading = ((isLoading) => {
-    if (isLoading){
+    if (isLoading) {
         const loadingElement = document.createElement('p')
         loadingElement.textContent = '取得中です...少々お待ちください'
 
         const loadingContainer = document.getElementById('loading')
         loadingContainer.appendChild(loadingElement)
     }
-    if (!isLoading){
+    if (!isLoading) {
         const loadingContainer = document.getElementById('loading')
         loadingContainer.innerHTML = ''
     }
 })
 
-const fetchAllCharacter = async () => {
-    handleLoading(true)
+// キャラデータ取得関数
+const fetchCharacter = async (e) => {
     try {
-        const res = await fetch('https://ihatov08.github.io/kimetsu_api/api/all.json');
-        if (!res.ok) throw new Error(`HTTP ${res.status}`);
-        const data = await res.json();
-        
-        const fetchDataContainer = document.getElementById('fetch-data-container')
-        fetchDataContainer.innerHTML = ''
-        data.forEach(item => {
-            const characterElement = document.createElement('div')
-            characterElement.innerHTML = `
+        // eの値のあるなしで初回ローディングかどうかを判定しています
+        handleLoading(true)
+        if (e.target.value) {
+            const res = await fetch(`https://ihatov08.github.io/kimetsu_api/api/${e.target.value}.json`);
+            if (!res.ok) throw new Error(`HTTP ${res.status}`);
+            const data = await res.json();
+
+            const fetchDataContainer = document.getElementById('fetch-data-container')
+            fetchDataContainer.innerHTML = ''
+            data.forEach(item => {
+                const characterElement = document.createElement('div')
+                characterElement.innerHTML = `
             <h2>${item.name}</h2>
             <img src="https://ihatov08.github.io${item.image}" alt="arunbababa">
             <p>${item.category}</p>
             `
-            fetchDataContainer.appendChild(characterElement)
-        })
+                fetchDataContainer.appendChild(characterElement)
+            })
+        }
+        else {
+            const res = await fetch(`https://ihatov08.github.io/kimetsu_api/api/all.json`);
+            if (!res.ok) throw new Error(`HTTP ${res.status}`);
+            const data = await res.json();
+
+            const fetchDataContainer = document.getElementById('fetch-data-container')
+            fetchDataContainer.innerHTML = ''
+            data.forEach(item => {
+                const characterElement = document.createElement('div')
+                characterElement.innerHTML = `
+            <h2>${item.name}</h2>
+            <img src="https://ihatov08.github.io${item.image}" alt="arunbababa">
+            <p>${item.category}</p>
+            `
+                fetchDataContainer.appendChild(characterElement)
+            })
+        }
+
     } catch (err) {
         console.error('データ取得エラー:', err);
     } finally {
@@ -42,86 +62,17 @@ const fetchAllCharacter = async () => {
 
 // 全キャラクター
 const ALL_RADIO = document.getElementById('all');
-window.addEventListener('DOMContentLoaded', fetchAllCharacter);
-ALL_RADIO.addEventListener('click', fetchAllCharacter);
+window.addEventListener('DOMContentLoaded', fetchCharacter); // ローディング時に実行
+ALL_RADIO.addEventListener('click', fetchCharacter);
 
 // 鬼殺隊
-const DEVILHUNTER_RADIO = document.getElementById('devil-hunter');
-DEVILHUNTER_RADIO.addEventListener('click', async () => {
-    try {
-        handleLoading(true)
-        const res = await fetch('https://ihatov08.github.io/kimetsu_api/api/kisatsutai.json');
-        if (!res.ok) throw new Error(`HTTP ${res.status}`);
-        const data = await res.json();
-        
-        const fetchDataContainer = document.getElementById('fetch-data-container')
-        fetchDataContainer.innerHTML = ''
-        data.forEach(item => {
-            const characterElement = document.createElement('div')
-            characterElement.innerHTML = `
-            <h2>${item.name}</h2>
-            <img src="https://ihatov08.github.io${item.image}" alt="arunbababa">
-            <p>${item.category}</p>
-            `
-            fetchDataContainer.appendChild(characterElement)
-        })
-    } catch (err) {
-        console.error('データ取得エラー:', err);
-    } finally {
-        handleLoading(false)
-    }
-});
+const DEVILHUNTER_RADIO = document.getElementById('kisatsutai');
+DEVILHUNTER_RADIO.addEventListener('click', fetchCharacter);
 
 // 柱
 const HASHIRA_RADIO = document.getElementById('hashira');
-HASHIRA_RADIO.addEventListener('click', async () => {
-    try {
-        handleLoading(true)
-        const res = await fetch('https://ihatov08.github.io/kimetsu_api/api/hashira.json');
-        if (!res.ok) throw new Error(`HTTP ${res.status}`);
-        const data = await res.json();
-        
-        const fetchDataContainer = document.getElementById('fetch-data-container')
-        fetchDataContainer.innerHTML = ''
-        data.forEach(item => {
-            const characterElement = document.createElement('div')
-            characterElement.innerHTML = `
-            <h2>${item.name}</h2>
-            <img src="https://ihatov08.github.io${item.image}" alt="arunbababa">
-            <p>${item.category}</p>
-            `
-            fetchDataContainer.appendChild(characterElement)
-        })
-    } catch (err) {
-        console.error('データ取得エラー:', err);
-    } finally {
-        handleLoading(false)
-    }
-});
+HASHIRA_RADIO.addEventListener('click', fetchCharacter);
 
 // 鬼
 const DEVIL_RADIO = document.getElementById('oni');
-DEVIL_RADIO.addEventListener('click', async () => {
-    try {
-        handleLoading(true)
-        const res = await fetch('https://ihatov08.github.io/kimetsu_api/api/oni.json');
-        if (!res.ok) throw new Error(`HTTP ${res.status}`);
-        const data = await res.json();
-        
-        const fetchDataContainer = document.getElementById('fetch-data-container')
-        fetchDataContainer.innerHTML = ''
-        data.forEach(item => {
-            const characterElement = document.createElement('div')
-            characterElement.innerHTML = `
-            <h2>${item.name}</h2>
-            <img src="https://ihatov08.github.io${item.image}" alt="arunbababa">
-            <p>${item.category}</p>
-            `
-            fetchDataContainer.appendChild(characterElement)
-        })
-    } catch (err) {
-        console.error('データ取得エラー:', err);
-    } finally {
-        handleLoading(false)
-    }
-});
+DEVIL_RADIO.addEventListener('click', fetchCharacter);
